171a7033911a6ce15c137575b6d84d922fa2305c
James Traver <jtraverjr@gmail.com>
2016-03-31 00:48:26 -0500

    accessing components like the dom and playing audio

--- app/src/main/java/org/androidlayoutplayground/codeforcoffee/thebestthings/MainActivity.java
+++ app/src/main/java/org/androidlayoutplayground/codeforcoffee/thebestthings/MainActivity.java
@@ -1,31 +1,73 @@
 package org.androidlayoutplayground.codeforcoffee.thebestthings;
 
+import android.content.Context;
+import android.content.res.AssetManager;
 import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.media.MediaPlayer;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
+import android.os.Handler;
+import android.widget.RelativeLayout;
 
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageView img = (ImageView) findViewById(R.id.imageView);
-    private TypedArray images = getResources().obtainTypedArray(R.array.imgs);
+    private Integer inc = 5;
+    private Handler handler;
+    private ImageView img;
+    private RelativeLayout rl;
+    //TypedArray images;
+    MediaPlayer mediaPlayer;
+    ArrayList<Integer> images = new ArrayList<Integer>();
+
 
     protected void onCreate(Bundle savedInstanceState) {
+
+        //images = getResources().obtainTypedArray(R.array.random_imgs);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        this.playMagdarTheme();
-    }
+        rl = (RelativeLayout) findViewById(R.id.main_relativelayout);
 
-    private void loopThroughImages() {
+        images.add(R.id.imgViewTrogdor);
+        images.add(R.id.imgViewPeench);
+        images.add(R.id.imgViewMagda);
+        images.add(R.id.imgViewPoutine);
+        images.add(R.id.imgViewTuna);
+        images.add(R.id.imgViewUs);
 
-    }
-
-    private void playMagdarTheme() {
-        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.drawable.madgar);
+        handler = new Handler();
+        mediaPlayer = MediaPlayer.create(this, R.raw.madgar);
         mediaPlayer.start();
+
+        Runnable task = new Runnable() {
+            public void run() {
+                Log.i("loop image", inc.toString());
+                img = (ImageView) findViewById(images.get(inc));
+                rl.removeView(img);
+                inc--;
+                if (inc < 0) {
+                    moveTaskToBack(true);
+                    android.os.Process.killProcess(android.os.Process.myPid());
+                    System.exit(1);
+                }
+                handler.postDelayed(this, 2700);
+            }
+        };
+        handler.postDelayed(task, 2700);
+
     }
+
 }
